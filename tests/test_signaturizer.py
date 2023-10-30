9fbb15ee860fc3e3c92105fef98bda107741d3fc
Aksel Lenes <aksel.lenes@irbbarcelona.org>
2023-10-30 15:08:54 +0100

    Add basic test for infering signature from coordinates
    This is a rough "integration test"
    to verify that the core signaturizer functionality survives being
    picked out of unimol_tools and put in this package without all the
    extra code and dependencies.

---
+++ tests/test_signaturizer.py
@@ -0,0 +1,31 @@
+import pickle
+from pathlib import Path
+
+import numpy as np
+import pytest
+
+from signaturizer3d.infer_signature import infer_from_coordinates
+
+
+def test_sig_inference_from_coordinates(atoms_coords_sig_single):
+    atoms_list, coordinates_list, expected_sig = atoms_coords_sig_single
+
+    result = infer_from_coordinates(atoms_list, coordinates_list)
+
+    assert result is not None
+    assert np.allclose(result, expected_sig, atol=1e-8)
+
+
+@pytest.mark.skip(reason="Takes too long")
+def test_sig_inference_from_coordinates_full():
+    data_dir = Path.cwd() / "tests" / "data"
+    with open(data_dir / "atoms_list.pkl", "rb") as f:
+        atoms_list = pickle.load(f)
+    with open(data_dir / "coordinates_list.pkl", "rb") as f:
+        coordinates_list = pickle.load(f)
+    with open(data_dir / "expected_sig4_output.pkl", "rb") as f:
+        expected_sigs = pickle.load(f)
+
+    result = infer_from_coordinates(atoms_list, coordinates_list)
+
+    assert np.allclose(result, expected_sigs, atol=1e-8)
