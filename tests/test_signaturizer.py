3e5d19360d84605055e96099d1139795bf1b6985
Aksel Lenes <aksel.lenes@irbbarcelona.org>
2023-10-31 14:47:37 +0100

    Test that inference is repetable and type signature shape

--- tests/test_signaturizer.py
+++ tests/test_signaturizer.py
@@ -3,19 +3,18 @@ from pathlib import Path
 
 import numpy as np
 
-from signaturizer3d.infer_signature import infer_from_coordinates
 
-
-def test_sig_inference_from_coordinates(atoms_coords_sig_single):
+def test_sig_inference_from_coordinates(signaturizer, atoms_coords_sig_single):
     atoms_list, coordinates_list, expected_sig = atoms_coords_sig_single
 
-    result = infer_from_coordinates(atoms_list, coordinates_list)
+    result = signaturizer.infer_from_coordinates(atoms_list, coordinates_list)
 
     assert result is not None
+    assert result.shape == (1, 128)
     assert np.allclose(result, expected_sig, atol=1e-6)
 
 
-def test_sig_inference_from_coordinates_full():
+def test_sig_inference_from_coordinates_full(signaturizer):
     data_dir = Path.cwd() / "tests" / "data"
     with open(data_dir / "atoms_list.pkl", "rb") as f:
         atoms_list = pickle.load(f)
@@ -29,3 +28,13 @@ def test_sig_inference_from_coordinates_full():
     assert result is not None
     assert result.shape == (32, 128)
     assert np.allclose(result, expected_sigs, atol=1e-8)
+
+
+def test_is_inference_repetables(signaturizer, atoms_coords_sig_single):
+    atoms_list, coordinates_list, expected_sig = atoms_coords_sig_single
+
+    result1 = signaturizer.infer_from_coordinates(atoms_list, coordinates_list)
+
+    result2 = signaturizer.infer_from_coordinates(atoms_list, coordinates_list)
+
+    assert np.allclose(result1, result2)
