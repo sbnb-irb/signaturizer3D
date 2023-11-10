b9b957ccd2cf3902b821063c8c6ab73517f21196
Aksel Lenes <aksel.lenes@irbbarcelona.org>
2023-11-10 09:57:47 +0100

    Add basic tests for inference performance

--- signaturizer3d/unimol.py
+++ signaturizer3d/unimol.py
@@ -24,7 +24,7 @@ class FineTunedUniMol(object):
     def __init__(
         self,
         space: CCSpace,
-        remove_hs: bool = False,
+        remove_hs: bool = True,
         use_gpu: bool = True,
         use_local_weights: bool = False,
     ):
