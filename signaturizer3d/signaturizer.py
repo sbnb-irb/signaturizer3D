eb6f42a6b6a05e1f2afa086b0d28f45f0a241261
Aksel Lenes <aksel.lenes@irbbarcelona.org>
2023-11-10 18:03:03 +0100

    Infer signatures from sdf file or dir of sdf files

--- signaturizer3d/signaturizer.py
+++ signaturizer3d/signaturizer.py
@@ -1,6 +1,7 @@
 import numpy as np
 import numpy.typing as npt
 
+from signaturizer3d.data.sdf import gather_sdf_data
 from signaturizer3d.types import CCSpace
 from signaturizer3d.unimol import FineTunedUniMol
 
@@ -29,3 +30,13 @@ class Signaturizer:
     def infer_from_smiles(self, smiles_list: list[str]) -> npt.NDArray[np.float32]:
         sig_output = self.model.get_sig4_smiles(smiles_list)
         return sig_output
+
+    def infer_from_sdf(self, path: str) -> npt.NDArray[np.float32]:
+        """
+        Perform inference on molecules in an SDF file or all SDF files within a directory.
+
+        Parameters:
+        path (str): Path to an SDF file or a directory containing SDF files.
+        """
+        atoms_list, coords_list = gather_sdf_data(path)
+        return self.infer_from_coordinates(atoms_list, coords_list)
