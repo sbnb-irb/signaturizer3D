32c4de1d03378213445b674feda49e0e0ed44915
Aksel Lenes <aksel.lenes@irbbarcelona.org>
2023-11-13 09:48:51 +0100

    Move normalization to its own module

--- signaturizer3d/inference.py
+++ signaturizer3d/inference.py
@@ -1,38 +1,14 @@
-import pathlib
-import pickle
 from typing import TYPE_CHECKING
 
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
 
+from signaturizer3d.normalize.normalize import normalize_for_space
+
 if TYPE_CHECKING:
-    from signaturizer3d.space import CCSpace
     from signaturizer3d.model import MolDataset, UniMolModel
-
-PACKAGE_ROOT = pathlib.Path(__file__).resolve().parents[0]
-
-
-def normalize_output_for_space(
-    raw_outputs: torch.Tensor, space: "CCSpace"
-) -> torch.Tensor:
-    means = pickle.load(
-        open(
-            PACKAGE_ROOT / "space_means.pkl",
-            "rb",
-        )
-    )
-    stds = pickle.load(
-        open(
-            PACKAGE_ROOT / "space_stds.pkl",
-            "rb",
-        )
-    )
-
-    space_mean = torch.tensor(means[space.value], device=raw_outputs.device)
-    space_std = torch.tensor(stds[space.value], device=raw_outputs.device)
-    normalized_output = raw_outputs * space_std + space_mean
-    return normalized_output
+    from signaturizer3d.space import CCSpace
 
 
 def run_inference(
@@ -58,7 +34,7 @@ def run_inference(
         input_data = {k: v.to(device) for k, v in input_data.items()}
         with torch.no_grad():
             raw_outputs = model(**input_data, features_only=True)
-            normalized_output = normalize_output_for_space(raw_outputs, space=space)
+            normalized_output = normalize_for_space(raw_outputs, space=space)
             raw_outputs_list.append(normalized_output.cpu().numpy())
 
     raw_outputs_array = np.concatenate(raw_outputs_list, axis=0)
