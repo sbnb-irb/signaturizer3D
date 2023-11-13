2a952fb62fedabaa4cde9a494bfdfe626620594e
Aksel Lenes <aksel.lenes@irbbarcelona.org>
2023-11-13 09:37:11 +0100

    Rename models/ -> unimol/

--- signaturizer3d/model.py
+++ signaturizer3d/model.py
@@ -4,7 +4,7 @@ from torch.utils.data import Dataset
 
 from signaturizer3d.data import coordinates_list_to_unimol, smiles_to_unimol
 from signaturizer3d.inference import run_inference
-from signaturizer3d.models import UniMolModel
+from signaturizer3d.unimol import UniMolModel
 from signaturizer3d.space import CCSpace
 
 
