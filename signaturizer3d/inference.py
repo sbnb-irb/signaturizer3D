9553fdbd966e31652216ddc843df9b25d34582a3
Aksel Lenes <aksel.lenes@irbbarcelona.org>
2023-11-27 13:53:25 +0100

    Add progress bar and logging for inference

--- signaturizer3d/inference.py
+++ signaturizer3d/inference.py
@@ -1,14 +1,18 @@
+import logging
 from typing import TYPE_CHECKING
 
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 from signaturizer3d.normalize.normalize import normalize_for_space
 
 if TYPE_CHECKING:
     from signaturizer3d.model import MolDataset, UniMolModel
     from signaturizer3d.space import CCSpace
+
+logger = logging.getLogger(__name__)
 
 
 def run_inference(
@@ -29,7 +33,8 @@ def run_inference(
     )
     model = model.eval()
     raw_outputs_list = []
-    for batch in dataloader:
+    logging.info(f"{space.name} starting inference")
+    for batch in tqdm(dataloader):
         input_data, _ = batch
         input_data = {k: v.to(device) for k, v in input_data.items()}
         with torch.no_grad():
@@ -37,5 +42,6 @@ def run_inference(
             normalized_output = normalize_for_space(raw_outputs, space=space)
             raw_outputs_list.append(normalized_output.cpu().numpy())
 
+    logging.info(f"{space.name} finished inference")
     raw_outputs_array = np.concatenate(raw_outputs_list, axis=0)
     return raw_outputs_array
